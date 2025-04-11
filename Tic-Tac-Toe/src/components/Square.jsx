52747bd307dab3ce464181a1f1fca22bc7617fac
Emmanuel Henrique de Faria Carvalho <e-carvalhoe@weg.net>
2025-04-11 14:35:36 -0300

    [feat] ✨: setup tic tac toe

---
+++ Tic-Tac-Toe/src/components/Square.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const Square = () => {
+  const [value, setValue] = useState(null);
+
+  const handleClick = () => {
+    setValue("X");
+  };
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
